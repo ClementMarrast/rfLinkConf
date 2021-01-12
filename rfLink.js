1094c9121303217ec1427179e75315005ee7240b
Clément Marrast <clement.marrast@zodiac.com>
2021-01-12 20:21:13 +0100

    SOMFY RTS: implement filter for getting all record
    Signed-off-by: Clément Marrast <clement.marrast@zodiac.com>

--- rfLink.js
+++ rfLink.js
@@ -23,7 +23,38 @@ rfLink.prototype.init = function()
   _this.rfLinkCom.connect();
 
   _this.rfLinkCom.on('receive', function(data){
-    _this.emit('receiveRawData', data);
+
+    _this.emit('rawData', data);
+    // Parse data
+    if(data.startsWith("20;")){
+      let dataObj = data.split(";")
+      dataObj.pop() // remove last empty item
+
+      let rfLinkObj = {
+        nodeNumber: dataObj[0],
+        packetCounter: dataObj[1],
+        name: dataObj[2],
+        dataArray: []
+      }
+
+      dataObj.forEach((devData, idx) => {
+        if(idx > 2){
+          devDataArray = devData.split("=")
+          rfLinkObj.dataArray.push({
+            name: devDataArray[0],
+            value: devDataArray[1]
+          })
+        }
+      });
+      
+      // console.log(rfLinkObj)
+      _this.emit('data', rfLinkObj);  
+    }else if(data.startsWith("RTS Record")){
+      console.log("rflink to somfy")
+      _this.emit("RTS-data", data)
+    }
+
+    
   });
 }
 
